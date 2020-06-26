5974cba7fe3c03e42961a802c7cf8374d838c548
Asita Chheda <asita.chheda@gmail.com>
2020-06-26 22:55:40 +0530

    c1

---
+++ add_devices.py
@@ -0,0 +1,61 @@
+import time
+import requests
+import json
+from auth_token import auth_token
+
+def main():
+    token = auth_token()
+
+    api_path = "https://sandboxdnac.cisco.com/dna"
+    headers = {"Content-Type": "application/json", "X-Auth-Token": token}
+
+    new_device_dict = {
+                    "ipAddress": ["192.0.2.1"],
+                     "snmpVersion": "v2",
+                     "snmpROCommunity": "readonly",
+                     "snmpRWCommunity": "readwrite",
+                     "snmpRetry": "1",
+                     "snmpTimeout": "60",
+                     "cliTransport": "ssh",
+                     "userName": "nick",
+                     "password": "secret123!",
+                     "enablePassword": "secret456!",
+     }
+
+    add_resp = requests.post( f"{api_path}/intent/api/v1/network-device",
+            json=new_device_dict,
+            headers=headers,
+            )
+
+
+
+    if add_resp.ok:
+        print(f"request code accepted: status code {add_resp.status_code}")
+
+        task = add_resp.json()["response"]["taskId"]
+        task_resp = requests.get( f"{api_path}/intent/v1/task/{task}", headers=headers )
+
+        if task_resp.ok:
+            task_data = task_resp.json()["response"]
+            if not task_data["isError"]:
+                print("new device added")
+            else:
+                print(f"async task error: {task_data['progress']}")
+        else:
+            print(f"asyn get failed: status code {task_resp.status_code}")
+    else:
+        print(f"device addition failed {add_resp.status_code}")
+        print(f"failure body: {add_resp.text}")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+        
+
+    
+                                                                    
+                                                                        
+                                                             
